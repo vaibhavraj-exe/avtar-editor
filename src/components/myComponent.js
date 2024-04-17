7c7428aeefe0de44264f1bb1e1bc018f17126280
vaibhavraj-exe <vraj.exe@gmail.com>
2024-04-17 22:05:26 +0530

    refactor

--- src/components/myComponent.js
+++
@@ -1,7 +0,0 @@
-export default function myComponent(){
-    return (
-      <div className="size-44 bg-blue-500 flex justify-center items-center p-5">
-        <h1>Image Processing Task</h1>
-      </div>
-    );
-}
